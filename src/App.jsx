3c881e52fcf6adc6faa627da2eafe6c97f4851fa
Rohit <rohitkumar@caplineservices.com>
2026-03-17 21:49:48 +0530

    ci done

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>Mern stack batch</h1>
        <h1>Hello,Rohit kumar</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
